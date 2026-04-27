5fe77d564b5db54a26524f2b457fbd48b060acf3
ttanya-melnik <tamara81tanucha@mail.ru>
2026-04-27 20:52:54 +0300

    .

--- src/main/java/mysya/cat/Main.java
+++ src/main/java/mysya/cat/Main.java
@@ -1,6 +1,7 @@
 package mysya.cat;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -12,21 +13,22 @@ public class Main {
   // Создали еще один лист (он будет сохранять результат нашей деятельности с потоком)
 
 
-    List<Person> persons2 = persons.stream()
-        .filter(p -> p.getAge() > 30)
-        .sorted()
-        .collect(Collectors.toList());
+  //  List<Person> persons2 = persons.stream()
+      //  .filter(p -> p.getAge() > 30)
+  //      .map(person -> new Person(person.getFirstName(), person.getLastName(), person.getAge() + 100))
+      //  .sorted()
+  //      .collect(Collectors.toList());
 
-
-
-
-
+    Optional<Person> person = persons.stream()
+        .filter(p -> p.getAge() > 18)
+    //    .findFirst();
+        .findAny();
+   System.out.println(person.get());
 
 
     // Создадим цикл
-    for(int i = 0 ; i < persons2.size() ; i++){
-      System.out.println(persons2.get(i));
+  //  for(int i = 0 ; i < persons2.size() ; i++){
+  //    System.out.println(persons2.get(i));}
     }
 
   }
-}
